e44ba1af901b8da2b63824ed43b6c378c1930a3f
tuannh982 <tuannh982@gmail.com>
2023-06-08 10:22:41 +0700

    update

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -3,12 +3,11 @@ require 'core.plugins.plugin'
 
 local manager = PluginManager:new {}
 manager:install({
-    require 'plugins.telescope',
-    require 'plugins.lsp':dependsOn { require 'plugins.treesitter' },
-    require 'plugins.filetree':dependsOn { require 'plugins.gitsigns' },
-    require 'plugins.bar':dependsOn { require 'plugins.gitsigns' },
-    require 'plugins.diffview',
-    Plugin:new { load_fn = function(use) use 'tpope/vim-sleuth' end },
-    require 'plugins.theme'
+	require 'plugins.lsp':dependsOn { require 'plugins.telescope', require 'plugins.treesitter' },
+	require 'plugins.filetree':dependsOn { require 'plugins.gitsigns' },
+	require 'plugins.bar':dependsOn { require 'plugins.gitsigns' },
+	require 'plugins.diffview',
+	Plugin:new { load_fn = function(use) use 'tpope/vim-sleuth' end },
+	require 'plugins.theme'
 
 })
